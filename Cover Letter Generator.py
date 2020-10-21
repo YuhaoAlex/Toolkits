6c187c45e5f5455d99653ca1f1c3fbf484270251
Yuhao Zhang <julyzyh@gmail.com>
2020-10-20 22:18:23 -0700

    Delete Cover Letter Generator.py

--- Cover Letter Generator.py
+++
@@ -1,6 +0,0 @@
-companyname = "The Accuro Group"
-position = "Business Analyst"
-
-text = "Dear Hiring Manager,\n\nIn my pursuit of new creative endeavors, I was excited to find the {} opportunity with {} Company. As a progressive professional with data science and analyst intern experience, I believe that I can bring valuable contributions to your team.\nFormerly in my Data Scientist Intern role, my successes have stemmed from essential skills. I built a data pipeline using Python and SQL, which connects multiple data sources to the AWS cloud database. I'm also proficient in data processing and visualization tools like Excel and Tableau. By conducting A/B testing, I helped my employer to launch some changes which brought a considerable revenue increase. I am a collaborative team player who is always searching for opportunities to impart valuable insights. With these qualities, I am able to facilitate positive change and impacting outcomes.\nFor a greater presentation of my background and qualifications, please review my attached resume. I am eager to speak with you and greatly appreciate your consideration.\n\nSincerely,\nYuhao Zhang".format(position, companyname)
-
-print(text)
